90a9584566583682dfc0772f28809349583d2fb4
thram <thramposo@gmail.com>
2015-07-31 01:07:09 +1200

    - Implement browser-sync for testing

--- gulpfile.js
+++ gulpfile.js
@@ -6,43 +6,11 @@ var gulp        = require('gulp'),
     rename      = require('gulp-rename'),
     uglify      = require('gulp-uglify'),
     jshint      = require('gulp-jshint'),
-    serve       = require('gulp-serve'),
-    compass     = require('gulp-compass'),
-    notify      = require('gulp-notify'),
-    livereload  = require('gulp-livereload'),
-    fileinclude = require('gulp-file-include'),
-    plumber     = require('gulp-plumber'),
+    browserSync = require('browser-sync').create(),
+    fileInclude = require('gulp-file-include'),
     gzip        = require('gulp-gzip'),
     del         = require('del'),
-    sync        = require('gulp-sync')(gulp),
-    path        = require('path');
-
-gulp.task('usemin', function () {
-    gulp.src('client/templates/*.html')
-        .pipe(usemin())
-        .pipe(gulp.dest('public/templates'));
-    gulp.src('client/templates/views/*.html')
-        .pipe(usemin())
-        .pipe(gulp.dest('public/templates/views'));
-    return gulp.src('client/templates/components/*.html')
-        .pipe(usemin())
-        .pipe(gulp.dest('public/templates/components'));
-});
-
-//the title and icon that will be used for the Grunt notifications
-var notifyInfo = {
-    title: 'Gulp',
-    icon : path.join(__dirname, 'gulp.png')
-};
-
-//error notification settings for plumber
-var plumberErrorHandler = {
-    errorHandler: notify.onError({
-        title  : notifyInfo.title,
-        icon   : notifyInfo.icon,
-        message: "Error: <%= error.message %>"
-    })
-};
+    sync        = require('gulp-sync')(gulp);
 
 gulp.task('clean', function (cb) {
     del(['dist/*'], cb);
@@ -65,42 +33,34 @@ gulp.task('build', function () {
         .pipe(gulp.dest('dist'));
 });
 
-var sass = require('gulp-sass');
-
-gulp.task('fonts', function () {
-    gulp.src('src/stylesheets/fonts/*')
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe(gulp.dest('example/fonts'));
-});
-
-//styles
-gulp.task('styles', function () {
-    return gulp.src(['src/stylesheets/playground.scss'])
-        .pipe(plumber(plumberErrorHandler))
-        .pipe(compass({
-            sass : 'src/stylesheets',
-            image: 'src/images'
-        }))
-        .pipe(gulp.dest('example/css'));
-});
-
-gulp.task('styles:watch', function () {
-    gulp.watch(['src/stylesheets/playground.scss', 'src/stylesheets/**/*.scss'], ['styles']);
-});
-
-gulp.task('watch', function () {
-    gulp.watch(['src/scripts/thram.js', 'src/scripts/**/*.js'], ['dist']);
-});
-
 gulp.task('templates', function () {
     gulp.src(['src/example/index.html'])
-        .pipe(fileinclude({
+        .pipe(fileInclude({
             template: '<script type="text/template" id="@filename"> @content </script>'
         }))
         .pipe(gulp.dest('example/'));
 });
 
-gulp.task('serve', serve(['example']));
+gulp.task('watch', function () {
+    var src = {
+        html: 'src/example/**/*.html',
+        js  : 'example/example.js'
+    };
+
+    gulp.watch([src.html], ['templates']);
+    gulp.watch([src.js], ['build']);
+    gulp.watch([src.html, src.js]).on('change', browserSync.reload);
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server        : {
+            baseDir: 'example'
+        },
+        logLevel      : 'debug',
+        logConnections: true
+    });
+});
 
 gulp.task('dist', sync.sync(['clean', ['lint', 'build']]));
-gulp.task('server', sync.sync(['clean', ['templates', 'lint', 'styles', 'fonts', 'build', 'serve', 'watch', 'styles:watch']]));
+gulp.task('server', sync.sync(['clean', ['templates', 'lint', 'build', 'browser-sync', 'watch']]));
